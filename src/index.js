77584c0880ed3d61aa89ab2d477796c20578a3e6
Sebastian Contreras <sacm1046@gmail.com>
2020-01-24 15:54:35 -0300

    presentacion1final

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-calendar/dist/Calendar.css';
 import './index.css';
 import Layout from './layout';
 import * as serviceWorker from './serviceWorker';
